ee1644d79067d6108766a36d2bdc18a93ce3b2d7
r_nishio <quick.xp@me.com>
2016-06-12 01:52:16 +0900

    各種マスタ スキーマ作成

---
+++ app/models/master/inv_type.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: inv_types
+#
+#  id              :integer          not null, primary key
+#  type_id         :integer          not null
+#  group_id        :integer
+#  type_name       :string(255)
+#  description     :text(65535)
+#  mass            :float(53)
+#  volume          :float(53)
+#  base_price      :decimal(20, 4)
+#  market_group_id :integer
+#
+
+class InvType < ActiveRecord::Base
+
+end
