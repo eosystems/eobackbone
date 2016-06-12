0db0df9fc626d085e51aa9255eb522da4784cb40
r_nishio <quick.xp@me.com>
2016-06-12 23:34:04 +0900

    コープ情報格納

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,6 +11,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     session[:expires_at] = auth["credentials"]["expires_at"]
     session[:refresh_token] = auth["credentials"]["refresh_token"]
     session[:character] = User.get_character_account_read(get_token, @user.uid)
+    # corp 初回
+    if Corp.find_by_corporation_id(session[:character].corporation_id).nil? && session[:character].corporation_id.present?
+      corp = Corp.new
+      corp.corporation_id = session[:character].corporation_id
+      corp.corporation_name = session[:character].corporation_name
+      corp.save!
+    end
     if @user.persisted?
         sign_in_and_redirect @user, :event => :authentication
         set_flash_message(:notice, :success, :kind => "Eve Online") if is_navigational_format?
