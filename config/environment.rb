f55d70e4ebf78f02eb5b164955ad640cec958694
r_nishio <quick.xp@me.com>
2016-07-31 17:05:42 +0900

    deploy中のエラー解消

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ class Logger
         msg = msg.gsub(/\n/, '')
         if msg.present? && !((msg.include?('assets') || msg.include?('erb')) && severity == 'ERROR')
           format = "[%s %d] %5s -- %s: \'%s\'\n"
-          format % ["#{time.strftime('%Y-%m-%dT%H:%M:%S')}.#{'%06d' % time.usec.to_s}", $PROCESS_ID, severity, progname, msg2str(msg)]
+          format % ["#{time.strftime('%Y-%m-%dT%H:%M:%S')}.#{'%06d' % time.usec.to_s}", $$, severity, progname, msg2str(msg)]
         end
       end
     end
