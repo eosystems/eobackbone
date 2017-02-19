6692c3c3a5da379e88a53352d3791aa0f467f259
r_nishio <quick.xp@me.com>
2017-02-19 21:03:33 +0900

    crawl ref types job

--- app/jobs/crawl_ref_types_job.rb
+++ app/jobs/crawl_ref_types_job.rb
@@ -3,6 +3,18 @@ class CrawlRefTypesJob < ActiveJob::Base
 
   def perform(*args)
     Rails.logger.info("start crawl ref types")
+    client = EveClient.new('dummy','dummy')
+    response = client.fetch_ref_types
+    items = response.items[0].rowset.row.map{ |v| HashObject.new(v) }
+
+    RefType.delete_all
+    items.each do |item|
+      r = RefType.new
+      r.id = item.refTypeID
+      r.name = item.refTypeName
+      r.save
+    end
+
     Rails.logger.info("end crawl ref types")
   end
 
