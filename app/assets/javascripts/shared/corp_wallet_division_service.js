020cae78fb1caf970f99831506a296ecf695b450
r_nishio <quick.xp@me.com>
2017-02-21 22:31:46 +0900

    corporation change 時にwallet divisionを再ロードするように修正

--- app/assets/javascripts/shared/corp_wallet_division_service.js
+++ app/assets/javascripts/shared/corp_wallet_division_service.js
@@ -5,15 +5,17 @@ angular.module('eoApp')
 .factory(
     'getCorpWalletDivision',
     function($q, $http) {
-        var deferred = $q.defer();
-
-        $http({
-                url: '/api/corp_wallet_divisions',
-                method: 'GET'
-            })
-            .success(function(res) {
-                deferred.resolve(res.results);
-            });
-        return deferred.promise;
+        return function(corporation_id) {
+            var deferred = $q.defer();
+            $http({
+                    url: '/api/corp_wallet_divisions',
+                    method: 'GET',
+                    params: {corporation_id: corporation_id}
+                })
+                .success(function(res) {
+                    deferred.resolve(res.results);
+                });
+            return deferred.promise;
+        };
     }
 );
--- app/controllers/api/corp_wallet_divisions_controller.rb
+++ app/controllers/api/corp_wallet_divisions_controller.rb
@@ -1,7 +1,9 @@
 class Api::CorpWalletDivisionsController < ApiController
   def index
+    corp = params[:corporation_id]
     @divisions = CorpWalletDivision
       .accessible_divisions(current_character.corporation_id)
+      .where(corporation_id: corp)
       .order(id: :asc)
   end
 
