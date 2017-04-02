af6698535287804f481a63b8918f12477b538a9d
r_nishio <quick.xp@me.com>
2017-04-02 20:53:05 +0900

    error対応

--- app/assets/javascripts/shared/corporation_service.js
+++ app/assets/javascripts/shared/corporation_service.js
@@ -5,15 +5,17 @@ angular.module('eoApp')
 .factory(
     'getCorporation',
     function($q, $http) {
-        var deferred = $q.defer();
+        return function(corporation_id) {
+            var deferred = $q.defer();
 
-        $http({
-                url: '/api/my_corporations',
-                method: 'GET'
-            })
-            .success(function(res) {
-                deferred.resolve(res.results);
-            });
-        return deferred.promise;
+            $http({
+                    url: '/api/my_corporations',
+                    method: 'GET'
+                })
+                .success(function(res) {
+                    deferred.resolve(res.results);
+                });
+            return deferred.promise;
+        };
     }
 );
