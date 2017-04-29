fa0ea5a6adac6ce7e3c71558fa8d33f0225836ba
r_nishio <quick.xp@me.com>
2017-04-30 00:09:07 +0900

    dev

--- tools/start_server.sh
+++ tools/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+cd /var/www/eobackbone/current
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec unicorn_rails -c config/unicorn.rb -E production -D
 tools/delayed_job_crawler.sh -e production start
