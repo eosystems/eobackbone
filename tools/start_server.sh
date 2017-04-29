25d1b1d69a5c70db41e71c14d2098d5448a6c6af
r_nishio <quick.xp@me.com>
2017-04-29 22:07:48 +0900

    Delayed job

--- tools/start_server.sh
+++ tools/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec unicorn_rails -c config/unicorn.rb -E production -D
-./delayed_job_crawler.sh -e production start
+tools/delayed_job_crawler.sh -e production start
