ebccaa22c7d1d888856b6d62c03a7edb9e9bfac6
r.nishio <quick.xp@gmail.com>
2018-06-16 18:28:35 +0900

    dev

--- app/models/corp_member.rb
+++ app/models/corp_member.rb
@@ -17,4 +17,23 @@
 #
 
 class CorpMember < ActiveRecord::Base
+  include NgTableSearchable
+  belongs_to :user, foreign_key: "character_id", primary_key: :uid
+  belongs_to :main_user, class_name: "User", foreign_key: "main_character_id", primary_key: :uid
+  belongs_to :corporation
+
+  RANSACK_FILTER_ATTRIBUTES = {
+    id: :id_eq_any,
+    character_name: :character_name_cont_any,
+    corporation_name: :corporation_name_cont_any,
+    main_character_name: :main_character_name_cont_any,
+    token_verify: :token_verify_eq_any,
+    token_error: :token_erro_eq_any
+  }.with_indifferent_access.freeze
+
+  def member_token_refresh
+    if user.present?
+
+    end
+  end
 end
