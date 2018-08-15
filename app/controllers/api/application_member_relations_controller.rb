33dbe512c8e90fef4a4213b805f9e8110a407c54
r.nishio <quick.xp@gmail.com>
2018-08-15 22:52:20 +0900

    メンバー紐付け申請画面

--- app/controllers/api/application_member_relations_controller.rb
+++ app/controllers/api/application_member_relations_controller.rb
@@ -9,8 +9,8 @@ class Api::ApplicationMemberRelationsController < ApiController
       return
     end
 
-    main = CorpMember.find_by(character_id: self.main_character_id)
-    sub = CorpMember.find_by(character_id: self.character_id)
+    main = CorpMember.find_by(character_id: current_user.uid)
+    sub = CorpMember.find_by(character_id: params['character_id'])
 
     if main.blank?
       render json: { error: "Mainキャラクターが見つかりません。先に新規メンバー登録をしてください" }, status: 404
@@ -22,13 +22,14 @@ class Api::ApplicationMemberRelationsController < ApiController
       return
     end
 
-    @application = Application.new(corporation: current_user.corporation)
+    @application = Application.new(corporation: current_user.user_detail.corporation)
     @application.targetable = ApplicationMemberRelation.new(
       character_id: sub.character_id,
       character_name: sub.character_name,
       main_character_id: main.character_id,
       main_character_name: main.character_name,
     )
+    @application.user = current_user
 
     if @application.save
       render json: { message: 'success'}
