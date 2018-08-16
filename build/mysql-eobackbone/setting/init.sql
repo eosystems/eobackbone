0cd19f955af421eae9ce80490ca7aeb4a288f84b
r.nishio <quick.xp@gmail.com>
2018-08-17 00:09:38 +0900

    kuroko2

--- build/mysql-eobackbone/setting/init.sql
+++ build/mysql-eobackbone/setting/init.sql
@@ -1,6 +1,11 @@
 create database eobackbone;
 create database eobackbone_development;
+create database kuroko;
+create database kuroko_development;
+
 
 CREATE USER eoservice;
 GRANT ALL ON eobackbone.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
 GRANT ALL ON eobackbone_development.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
+GRANT ALL ON kuroko.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
+GRANT ALL ON kuroko_development.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
